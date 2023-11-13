e4e54a2972d8dc2d069f0e95e7bebad826a9cd70
peng-zhou <peterzhou.syd@gmail.com>
2023-11-13 14:57:59 +1100

    cypress cloud setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "r7veaj",
   e2e: {
     baseUrl: "https://example.cypress.io",
     setupNodeEvents(on, config) {
